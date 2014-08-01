7aa248978174c3d5231f02a860abe7db178a4ad4
KrisMraz <mobdev2@kreyos.com>
2014-08-01 11:13:21 +0800

    Initial Commit

---
+++ Sortr/SortrAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  SortrAppDelegate.h
+//  Sortr
+//
+//  Created by KrisMraz on 8/1/14.
+//  Copyright (c) 2014 TheCoapperative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SortrAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Sortr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sortr
+//
+//  Created by KrisMraz on 8/1/14.
+//  Copyright (c) 2014 TheCoapperative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SortrAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SortrAppDelegate class]));
+    }
+}
